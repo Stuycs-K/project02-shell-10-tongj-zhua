d805f547bbd4632513b8000d53fe2dd7f6c4a628
Janet Tong <jtong50@stuy.edu>
2024-12-05 15:16:58 -0500

    redirect < > fix

--- redirection.c
+++ redirection.c
@@ -3,24 +3,42 @@
 #include <string.h>
 #include <unistd.h>
 #include <fcntl.h>
+// arguments: char * command is line with >
+// return: void
+// redirects stdout of left to right
+void rd_stdout(char * command){ // front text goes into back
+    char * front;
+    char * back = command;
+    front = strsep(&back, ">");
+    int fdback = open(back, O_WRONLY);
+    int backup_stdout = dup(STDOUT_FILENO);
+    char * arg_ary[2];
+    arg_ary[0]=front;
+    arg_ary[1]=NULL;
+    dup2(fdback, STDOUT_FILENO);
+    execvp(arg_ary[0], arg_ary);
+    dup2(backup_stdout, STDOUT_FILENO);
+}
+
 // arguments: char * command is line with <
 // return: void
-// redirects stdin of left to right
+// redirects stdout of left with stdin of right
 void rd_stdin(char * command){ // back text goes into front
     char * front;
     char * back = command;
     front = strsep(&back, "<");
     int fdback = open(back, O_WRONLY);
     int backup_stdin = dup(STDIN_FILENO);
-    int stdin = STDIN_FILENO;
     char * arg_ary[2];
     arg_ary[0]=front;
     arg_ary[1]=NULL;
-    dup2(STDIN_FILENO,fdback);
+    dup2(fdback, STDIN_FILENO);
     execvp(arg_ary[0], arg_ary);
-    dup2(STDIN_FILENO,backup_stdin);
+    dup2(backup_stdin, STDIN_FILENO);
 }
+
+
 int main(){
-    char  test[256]= "c.txt<ls";
-    rd_stdin(test);
+    char  test[256]= "ls>c.txt";
+    rd_stdout(test);
 }
